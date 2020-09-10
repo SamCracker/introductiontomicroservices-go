c7f2e9878688a4e8774f00d8b785636a92dd383b
samcracker <samcracker190897@gmail.com>
2020-09-11 03:31:27 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/samcracker/introductiontomicroservices-go/product-api
+
+go 1.15
+
+// require github.com/samcracker/env v0.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/samcracker/introductiontomicroserver-go/handlers"
+)
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
+func main() {
+	env.Parse()
+
+	l := log.New(os.Stdout, "products-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := http.Server{
+		Addr:         *bindAddress,
+		Handler:      sm,
+		Errorlog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+
+}
